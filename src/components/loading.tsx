1d15a68c438d55afee17d7dd00a5972df0396d2e
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2024-04-01 13:01:35 -0300

    (Version 1.0) Updating import

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,15 +1,11 @@
 import { useEffect, useState } from "react";
 import "./styles/loading.css";
 
-type Theme = "Dark" | "Light";
+import { Theme, colors } from "@/services/utils";
 
 export default function Loading() {
     const [currentTheme, setCurrentTheme] = useState<Theme>("Light");
 
-    const theme: Record<Theme, string[]> = {
-        "Light": ["bg-white"],
-        "Dark": ["bg-gray-700"]
-      }
     useEffect(() => {
         const storageTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") as Theme : "Light";
         setCurrentTheme(storageTheme);
@@ -27,8 +23,8 @@ export default function Loading() {
     }, []);
     return (
         <div style = {{height: "100vh", width: "100vw"}}>
-        <div className={`loadingBox ${theme[currentTheme][0]}`}>
-            <div className={`loading ${theme[currentTheme][0]}`}></div>
+        <div className={`loadingBox ${colors[currentTheme][0]}`}>
+            <div className={`loading ${colors[currentTheme][0]}`}></div>
         </div>
         </div>
     )
